2cf30e07d1b63e738168a3dc2bc0fa928c97d2a4
Toni Lammi <tangojulietlima@gmail.com>
2024-11-05 19:46:25 +0200

    better conversion

--- lib/src/media/frame_converter.cpp
+++ lib/src/media/frame_converter.cpp
@@ -9,10 +9,41 @@ extern "C" {
 
 namespace plai::media {
 namespace {
-AVPixelFormat pixel_conversion(AVPixelFormat in) {
+
+AVPixelFormat intermediate_pixel_fmt(AVPixelFormat in) {
     switch (in) {
-        case AV_PIX_FMT_YUVJ422P: return AV_PIX_FMT_YUV420P;
+        case AV_PIX_FMT_YUVJ420P: return AV_PIX_FMT_YUV420P;
+        case AV_PIX_FMT_YUVJ422P: return AV_PIX_FMT_YUV422P;
+        case AV_PIX_FMT_YUVJ440P: return AV_PIX_FMT_YUV440P;
+        case AV_PIX_FMT_YUVJ444P: return AV_PIX_FMT_YUV444P;
         default: return in;
+    }
+}
+
+void adjust_colorspace(SwsContext* ctx) {
+    int src_range{};
+    int dst_range{};
+    int brightness{};
+    int contrast{};
+    int saturation{};
+    int* dummy{};
+
+    sws_getColorspaceDetails(ctx, &dummy, &src_range, &dummy, &dst_range,
+                             &brightness, &contrast, &saturation);
+    src_range = 1;
+    const int* coefs = sws_getCoefficients(SWS_CS_DEFAULT);
+    sws_setColorspaceDetails(ctx, coefs, src_range, coefs, dst_range,
+                             brightness, contrast, saturation);
+}
+
+// modify the pixels so they are compatible with SDL
+AVPixelFormat output_pixel_format(AVPixelFormat intermediate) {
+    switch (intermediate) {
+        case AV_PIX_FMT_YUV420P:
+        case AV_PIX_FMT_YUV422P:
+        case AV_PIX_FMT_YUV440P:
+        case AV_PIX_FMT_YUV444P: return AV_PIX_FMT_YUV420P;
+        default: return intermediate;
     }
 }
 }  // namespace
@@ -24,12 +55,14 @@ FrameConverter::~FrameConverter() {
 Frame FrameConverter::operator()(Vec<int> dst_dims, const Frame& src,
                                  Frame dst) {
     auto pix_fmt = static_cast<AVPixelFormat>(src.raw()->format);
-    auto out_pix_fmt = pixel_conversion(pix_fmt);
+    auto intermediate_fmt = intermediate_pixel_fmt(pix_fmt);
+    auto out_pix_fmt = output_pixel_format(intermediate_fmt);
     auto src_dims = src.dims();
-    m_ctx = sws_getCachedContext(m_ctx, src_dims.x, src_dims.y, pix_fmt,
-                                 dst_dims.x, dst_dims.y, out_pix_fmt, 0,
-                                 nullptr, nullptr, nullptr);
+    m_ctx = sws_getCachedContext(m_ctx, src_dims.x, src_dims.y,
+                                 intermediate_fmt, dst_dims.x, dst_dims.y,
+                                 out_pix_fmt, 0, nullptr, nullptr, nullptr);
     if (!m_ctx) throw ValueError("Could not create libswscale context");
+    if (pix_fmt != intermediate_fmt) adjust_colorspace(m_ctx);
     auto* raw = dst.raw();
     raw->format = out_pix_fmt;
     raw->width = dst_dims.x;
