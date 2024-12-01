3c3358e2ce82c7155a4fd06d59da31a0df79063d
Toni Lammi <tangojulietlima@gmail.com>
2024-12-01 22:36:28 +0200

    autoformat+logs

--- lib/src/media/decoder.cpp
+++ lib/src/media/decoder.cpp
@@ -1,4 +1,5 @@
 #include <plai/exceptions.hpp>
+#include <plai/logs/logs.hpp>
 #include <plai/media/decoder.hpp>
 #include <plai/util/defer.hpp>
 
@@ -12,40 +13,41 @@ extern "C" {
 namespace plai::media {
 
 Decoder::Decoder() : m_ctx(avcodec_alloc_context3(nullptr)) {
-  if (!m_ctx) throw std::bad_alloc();
+    if (!m_ctx) throw std::bad_alloc();
 }
 
 Decoder::Decoder(StreamView str) : Decoder() {
-  AV_CHECK(avcodec_parameters_to_context(m_ctx, str.raw()->codecpar));
-  m_ctx->pkt_timebase = str.raw()->time_base;
-  auto* codec = avcodec_find_decoder(m_ctx->codec_id);
-  if (!codec) throw ValueError("Codec not found");
-  // TODO: codec options
-  AV_CHECK(avcodec_open2(m_ctx, codec, nullptr));
+    AV_CHECK(avcodec_parameters_to_context(m_ctx, str.raw()->codecpar));
+    m_ctx->pkt_timebase = str.raw()->time_base;
+    auto* codec = avcodec_find_decoder(m_ctx->codec_id);
+    if (!codec) throw ValueError("Codec not found");
+    PLAI_DEBUG("using codec: {}", codec->long_name);
+    // TODO: codec options
+    AV_CHECK(avcodec_open2(m_ctx, codec, nullptr));
 }
 
 Decoder::Decoder(Decoder&& other) noexcept
     : m_ctx(std::exchange(other.m_ctx, nullptr)) {}
 
 Decoder& Decoder::operator=(Decoder&& other) noexcept {
-  auto tmp = Decoder(m_ctx);
-  m_ctx = std::exchange(other.m_ctx, nullptr);
-  return *this;
+    auto tmp = Decoder(m_ctx);
+    m_ctx = std::exchange(other.m_ctx, nullptr);
+    return *this;
 }
 
 Decoder::~Decoder() {
-  if (m_ctx) avcodec_free_context(&m_ctx);
+    if (m_ctx) avcodec_free_context(&m_ctx);
 }
 Decoder& Decoder::operator<<(const Packet& pkt) {
-  AV_CHECK(avcodec_send_packet(m_ctx, pkt.raw()));
-  return *this;
+    AV_CHECK(avcodec_send_packet(m_ctx, pkt.raw()));
+    return *this;
 }
 
 bool Decoder::operator>>(Frame& frm) {
-  int res = avcodec_receive_frame(m_ctx, frm.raw());
-  if (res >= 0) return true;
-  if (res == AVERROR(EAGAIN)) return false;
-  throw AVException(res);
+    int res = avcodec_receive_frame(m_ctx, frm.raw());
+    if (res >= 0) return true;
+    if (res == AVERROR(EAGAIN)) return false;
+    throw AVException(res);
 }
 int Decoder::width() const noexcept { return m_ctx->width; }
 int Decoder::height() const noexcept { return m_ctx->height; }
