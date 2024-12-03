26a3f05d86a2d67ccdc15dd76ea9798d558b5b2e
Toni Lammi <tangojulietlima@gmail.com>
2024-12-03 13:49:04 +0200

    autoformat

--- lib/include/plai/exceptions.hpp
+++ lib/include/plai/exceptions.hpp
@@ -6,18 +6,19 @@ namespace plai {
 
 class Exception : public std::runtime_error {
  protected:
-  Exception() noexcept : std::runtime_error("") {}
-  template <class T>
-  Exception(T&& t) noexcept : std::runtime_error(std::forward<T>(t)) {}
+    Exception() noexcept : std::runtime_error("") {}
+    template <class T>
+    Exception(T&& t) noexcept : std::runtime_error(std::forward<T>(t)) {}
 
  private:
 };
 
 class ValueError : public Exception {
  public:
-  ValueError(const char* msg) noexcept : Exception(msg) {}
-  ValueError(const std::string& s) noexcept : Exception(s) {}
+    ValueError(const char* msg) noexcept : Exception(msg) {}
+    ValueError(const std::string& s) noexcept : Exception(s) {}
 
  private:
 };
+
 }  // namespace plai
