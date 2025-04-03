9efbdc80f2da8d67ed122d620f434785cb93b00f
Toni Lammi <tangojulietlima@gmail.com>
2025-04-03 15:38:48 +0300

    dev scripts

---
+++ script/client.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ucurl() {
+  curl --unix-socket /tmp/plai.sock "${@}"
+}
+
+is_vid() {
+  [[ $1 == *.mp4 ]]
+}
+
+type() {
+  if is_vid "$1"; then
+    printf "video"
+  else
+    printf "image"
+  fi
+}
+
+upload() {
+  nm="${1}"
+  shift
+  path="$(realpath "${1}")"
+  typ="$(type "$path")"
+  ucurl -X PUT "http://_/plai/v1/media/${typ}/${nm}" --data-binary "@${path}" >/dev/null
+  echo "${typ}/${nm}"
+}
+
+play() {
+  data="${1}"
+  ucurl "http://_/plai/v1/play?replay=true" -d "${data}"
+}
+
+cmd="${1}"
+shift
+
+"$cmd" "${@}"
---
+++ script/plai.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+
+build/bin/plai -s /tmp/plai.sock
