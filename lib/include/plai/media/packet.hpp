20de58625e6048014db20cc447c4383b3852a29b
Toni Lammi <tangojulietlima@gmail.com>
2025-04-03 15:11:47 +0300

    autoformat

--- lib/include/plai/media/packet.hpp
+++ lib/include/plai/media/packet.hpp
@@ -8,30 +8,30 @@ namespace plai::media {
 
 class Packet {
  public:
-  Packet();
-  constexpr explicit Packet(AVPacket* raw) noexcept : m_raw(raw) {}
+    Packet();
+    constexpr explicit Packet(AVPacket* raw) noexcept : m_raw(raw) {}
 
-  Packet(const Packet&) = delete;
-  Packet& operator=(const Packet&) = delete;
+    Packet(const Packet&) = delete;
+    Packet& operator=(const Packet&) = delete;
 
-  Packet(Packet&& other) noexcept;
-  Packet& operator=(Packet&& other) noexcept;
+    Packet(Packet&& other) noexcept;
+    Packet& operator=(Packet&& other) noexcept;
 
-  ~Packet();
+    ~Packet();
 
-  size_t stream_index() const noexcept;
+    size_t stream_index() const noexcept;
 
-  AVPacket* raw() noexcept { return m_raw; }
-  const AVPacket* raw() const noexcept { return m_raw; }
+    AVPacket* raw() noexcept { return m_raw; }
+    const AVPacket* raw() const noexcept { return m_raw; }
 
-  std::span<const uint8_t> data() const noexcept;
+    std::span<const uint8_t> data() const noexcept;
 
-  size_t size() const noexcept;
+    size_t size() const noexcept;
 
-  explicit operator bool() const noexcept { return size(); }
+    explicit operator bool() const noexcept { return size(); }
 
  private:
-  AVPacket* m_raw;
+    AVPacket* m_raw;
 };
 
 }  // namespace plai::media
