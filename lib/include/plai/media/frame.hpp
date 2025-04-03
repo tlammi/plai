0e77ee218f765878824abd1eaeb09c4d536356c4
Toni Lammi <tangojulietlima@gmail.com>
2025-04-03 15:10:19 +0300

    dynamic storage support + autoformat

--- lib/include/plai/media/frame.hpp
+++ lib/include/plai/media/frame.hpp
@@ -6,30 +6,40 @@
 namespace plai::media {
 class Frame {
  public:
-  Frame();
-  constexpr explicit Frame(AVFrame* raw) noexcept : m_raw(raw) {}
+    Frame();
+    constexpr explicit Frame(AVFrame* raw) noexcept : m_raw(raw) {}
 
-  Frame(const Frame&) = delete;
-  Frame& operator=(const Frame&) = delete;
+    Frame(const Frame&) = delete;
+    Frame& operator=(const Frame&) = delete;
 
-  Frame(Frame&& other) noexcept;
-  Frame& operator=(Frame&& other) noexcept;
+    Frame(Frame&& other) noexcept;
+    Frame& operator=(Frame&& other) noexcept;
 
-  ~Frame();
+    ~Frame();
 
-  [[nodiscard]] AVFrame* raw() noexcept { return m_raw; }
-  [[nodiscard]] const AVFrame* raw() const noexcept { return m_raw; }
+    [[nodiscard]] AVFrame* raw() noexcept { return m_raw; }
+    [[nodiscard]] const AVFrame* raw() const noexcept { return m_raw; }
 
-  /**
-   * \brief False for empty frames
-   * */
-  explicit operator bool() const noexcept;
+    /**
+     * \brief False for empty frames
+     * */
+    explicit operator bool() const noexcept;
 
-  int width() const noexcept;
-  int height() const noexcept;
-  Vec<int> dims() const noexcept { return {width(), height()}; }
+    int width() const noexcept;
+    int height() const noexcept;
+    Vec<int> dims() const noexcept { return {width(), height()}; }
+
+    // Implementation detail. Don't touch.
+    constexpr void is_dynamic(bool v) noexcept { m_is_dyn = v; }
+
+    constexpr bool is_dynamic() const noexcept { return m_is_dyn; }
 
  private:
-  AVFrame* m_raw;
+    AVFrame* m_raw;
+    // Hack to get libswscale working.
+    //
+    // It does some special memory allocation which means we need to do some
+    // special memory deallocation.
+    bool m_is_dyn{false};
 };
 }  // namespace plai::media
--- lib/src/media/frame.cpp
+++ lib/src/media/frame.cpp
@@ -12,16 +12,21 @@ Frame::Frame() : m_raw(av_frame_alloc()) {
     if (!m_raw) throw std::bad_alloc();
 }
 Frame::Frame(Frame&& other) noexcept
-    : m_raw(std::exchange(other.m_raw, nullptr)) {}
+    : m_raw(std::exchange(other.m_raw, nullptr)),
+      m_is_dyn(std::exchange(other.m_is_dyn, false)) {}
 
 Frame& Frame::operator=(Frame&& other) noexcept {
     auto tmp = Frame(std::move(other));
     std::swap(m_raw, tmp.m_raw);
+    std::swap(m_is_dyn, tmp.m_is_dyn);
     return *this;
 }
 
 Frame::~Frame() {
-    if (m_raw) av_frame_free(&m_raw);
+    if (m_raw) {
+        if (m_is_dyn) av_freep(&m_raw->data);
+        av_frame_free(&m_raw);
+    }
 }
 
 Frame::operator bool() const noexcept {
