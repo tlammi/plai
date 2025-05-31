dc83cbbe021179ae1ed5f737723d4f26a89c5c91
Toni Lammi <tangojulietlima@gmail.com>
2025-05-31 14:12:35 +0300

    more container support

--- containers/build.sh
+++ containers/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-set -eu
+set -e
+
+if [ $# -eq 0 ]; then
+  echo "usage: [podman flags...] envname"
+fi
 
 THISDIR="$(dirname ${BASH_SOURCE[0]})"
 dockerfile="$THISDIR/Dockerfile.${1}"
 
-podman build --arch=amd64 -t "plai-$1" -f "$dockerfile" "$THISDIR"
+opts=(${@:1:$(($# - 1))})
+container="${@: -1}"
+
+podman build ${opts} -t "plai-$container" -f "$THISDIR/Dockerfile.$container" "$THISDIR"
