d91c3eeffb80af1efd81c701e5667689696dc5ff
Toni Lammi <tangojulietlima@gmail.com>
2025-06-10 16:48:30 +0300

    utility for converting member function to lambda

---
+++ lib/include/plai/util/memfn.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <utility>
+
+namespace plai {
+
+template <class C, class R, class... Ps>
+constexpr auto memfn(C* ptr, R (C::*mthd)(Ps...)) noexcept {
+    return
+        [ptr, mthd](Ps... ps) { return ptr->*mthd(std::forward<Ps>(ps)...); };
+}
+
+}  // namespace plai
