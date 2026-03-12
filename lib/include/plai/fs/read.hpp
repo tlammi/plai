e59080b77acb744dbca820be46f9f91714f26480
Toni Lammi <tangojulietlima@gmail.com>
2026-03-12 20:54:26 +0200

    add chunked file read

--- lib/include/plai/fs/read.hpp
+++ lib/include/plai/fs/read.hpp
@@ -2,10 +2,13 @@
 
 #include <cstdint>
 #include <filesystem>
+#include <generator>
 #include <vector>
 
 namespace plai::fs {
 namespace stdfs = std::filesystem;
 
 std::vector<uint8_t> read_bin(const stdfs::path& path);
+std::generator<std::span<uint8_t>> read_chunked(const stdfs::path& path);
+
 }  // namespace plai::fs
--- lib/src/fs/read.cpp
+++ lib/src/fs/read.cpp
@@ -23,4 +23,25 @@ std::vector<uint8_t> read_bin(const stdfs::path& path) {
     assert(read_bytes == vec.size());
     return vec;
 }
+std::generator<std::span<uint8_t>> read_chunked(
+    const stdfs::path& path /*NOLINT(*-avoid-reference*)*/) {
+    static constexpr auto buf_size = 1024;
+    std::array<uint8_t, buf_size> buf{};
+    // NOLINTNEXTLINE
+    FILE* f = fopen(path.native().c_str(), "r");
+    if (!f) throw ValueError(plai::format("could not open {}", path.native()));
+    // NOLINTNEXTLINE
+    auto defer = Defer([&] { fclose(f); });
+
+    while (true) {
+        auto count = fread(buf.data(), sizeof(uint8_t), buf.size(), f);
+        if (count == buf.size()) co_yield buf;
+        if (std::feof(f)) {
+            co_yield buf;
+            co_return;
+        }
+        throw ValueError(plai::format("error reading file {}", path.native()));
+    }
+}
+
 }  // namespace plai::fs
