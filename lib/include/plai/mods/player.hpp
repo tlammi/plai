c67a82e3dd25ff0e17550c17db05eb7d108f6c94
Toni Lammi <tangojulietlima@gmail.com>
2026-03-13 19:08:42 +0200

    access MediaStream

--- lib/include/plai/mods/player.hpp
+++ lib/include/plai/mods/player.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <exec/start_detached.hpp>
+#include <plai/logs/logs.hpp>
 #include <plai/media2/media.hpp>
 #include <plai/mods/module.hpp>
 #include <print>
@@ -24,6 +25,11 @@ class Player : public Module {
     explicit Player(MediaStream& stream) : m_stream(&stream) {}
 
     void start(Context& ctx) override {
+        exec::start_detached(stdexec::on(
+            ctx.scheduler(),
+            m_stream->next_media() | stdexec::then([](auto media) {
+                PLAI_DEBUG("received media with size: {}", media.data().size());
+            })));
         exec::start_detached(stdexec::on(
             ctx.scheduler(),
             stdexec::just() | stdexec::then([&ctx]() { ctx.request_stop(); })));
