6e1e9e6a6df5f9b26ba1c64130deb8e1a3497add
Toni Lammi <tangojulietlima@gmail.com>
2026-03-28 08:54:35 +0200

    API documentation updates

--- doc/serve-html.sh
+++ doc/serve-html.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+THISDIR=$(dirname $(realpath $0))
+
 podman run -it --rm -p 8080:80 \
-  -v $(pwd):/usr/share/nginx/html/spec \
+  -v $THISDIR:/usr/share/nginx/html/spec \
   -e SPEC_URL=spec/api.yaml \
   docker.io/redocly/redoc
